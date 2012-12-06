e7692b6d29a31d574a5646396418ead862fe0ea7
Zark <zark@ubuntu.(none)>
2012-12-06 11:28:15 +0800

    *) 修改test/perf原子测试的代码

--- test/perf/chaos.cpp
+++ test/perf/chaos.cpp
@@ -9,12 +9,9 @@ int main(int argc_, char* argv_[])
 {
     NEW_SERVICE();
 
-    WS().start(WS_THREAD_NUM);
-
     test_atomic_perf();
 
     application_tool_t::wait_signal();
-    WS().stop();
 
     DEL_SERVICE();
 
--- test/perf/test_perf.h
+++ test/perf/test_perf.h
@@ -1,9 +1,10 @@
 #ifndef _CHAOS_TEST_PERF_H_
 #define _CHAOS_TEST_PERF_H_
 
-#define WS_THREAD_NUM   8
+#define WSG_THREAD_NUM   8
 #define USE_ATOMIC 1
-#define VAL_PER_THREAD 1
+#define VAL_PER_THREAD 0
+#define CALC_COUNT 100000000
 
 //! yunjie: 这里不能用数组, 性能会被cache line影响
 #if USE_ATOMIC
@@ -26,7 +27,9 @@ volatile uint32_t g_val6 = 0;
 volatile uint32_t g_val7 = 0;
 #endif
 
-void val_op(int index_)
+uint64_t cost_stat[WSG_THREAD_NUM] = {0};
+
+void val_op(int val_index_, int thd_index_)
 {
     struct timeval begin_tv;
     gettimeofday(&begin_tv, NULL);
@@ -37,7 +40,7 @@ void val_op(int index_)
         volatile uint32_t*
 #endif
         tmp_val;
-    switch (index_)
+    switch (val_index_)
     {
         case 0:
             tmp_val =
@@ -108,7 +111,7 @@ void val_op(int index_)
             break;
     }
 
-    for (uint64_t i = 0; i < 100000000; ++i)
+    for (uint64_t i = 0; i < CALC_COUNT; ++i)
     {
         (*tmp_val)++;
         (*tmp_val)--;
@@ -118,20 +121,32 @@ void val_op(int index_)
     gettimeofday(&end_tv, NULL);
     uint64_t cost_us = (end_tv.tv_sec - begin_tv.tv_sec) * 1000 * 1000 + (end_tv.tv_usec - begin_tv.tv_usec);
 
-    printf("cost us:[%lu]\n", cost_us);
+    cost_stat[thd_index_] = cost_us;
+
+    //! printf("cost us:[%lu]\n", cost_us);
 }
 
 void test_atomic_perf()
 {
+    WSG().start(WSG_THREAD_NUM);
+
     for (int i = 0; i < WSG().size(); ++i)
     {
-        int arg;
+        int val_i;
 #if VAL_PER_THREAD
-        arg = i;
+        val_i = i;
 #else
-        arg = 0;
+        val_i = 0;
 #endif
-        WSG()[i]->post(bindfunc(&val_op, arg));
+        WSG()[i]->post(bindfunc(&val_op, val_i, i));
+    }
+
+    WSG().stop();
+
+    for (int i = 0; i < WSG_THREAD_NUM; ++i)
+    {
+        uint32_t avg = CALC_COUNT / cost_stat[i];
+        printf("total count:[%lu] cost us:[%lu us] avg:[%d]\n", (uint64_t)CALC_COUNT, cost_stat[i], avg);
     }
 }
 
