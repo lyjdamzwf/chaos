cbc9b7798a1825bc58702fced30faa1c27b0d644
zark lu <zark@localhost.localdomain>
2013-11-11 22:47:39 +0800

    *) build.sh阶段性失败则退出

--- build.sh
+++ build.sh
@@ -17,30 +17,45 @@ fi
 echo "install directory:"$def_dir
 
 if [ "$build_part" = "all" ]; then
-cd chaos/deps/jemalloc
-sh autogen.sh --with-jemalloc-prefix=je_
-make -j${make_jobs}
-cd ../../../
+    cd chaos/deps/jemalloc
+    sh autogen.sh --with-jemalloc-prefix=je_
+    make -j${make_jobs}
+    mret=$(echo $?)
+    if [ $mret -ne 0 ]; then
+        echo "COMPILE ERROR!!!!!!!!!!!!!!!!!!!!!"
+        exit
+    fi
+    cd ../../../
 fi
 
 if [ "$build_part" = "all" ] || [ "$build_part" = "reconf" ]; then
-aclocal
-libtoolize
-autoconf
-autoheader
-automake --add-missing
-./configure --prefix=$def_dir
+    aclocal
+    libtoolize
+    autoconf
+    autoheader
+    automake --add-missing
+    ./configure --prefix=$def_dir
 fi
 
 if [ "$build_part" = "all" ] || [ "$build_part" = "reconf" ] || [ "$build_part" = "chaos" ]; then
-make -C chaos -j${make_jobs}
+    make -C chaos -j${make_jobs}
+    mret=$(echo $?)
+    if [ $mret -ne 0 ]; then
+        echo "COMPILE ERROR!!!!!!!!!!!!!!!!!!!!!"
+        exit
+    fi
 #cp ./chaos/utility/libchaos_utility.a ./chaos/utility/libchaos_utility_je.a 
 #cp ./chaos/libchaos.a ./chaos/libchaos_je.a 
-ar -q ./chaos/utility/libchaos_utility.a chaos/deps/jemalloc/src/*.o
-ar -q ./chaos/libchaos.a chaos/deps/jemalloc/src/*.o
+    ar -q ./chaos/utility/libchaos_utility.a chaos/deps/jemalloc/src/*.o
+    ar -q ./chaos/libchaos.a chaos/deps/jemalloc/src/*.o
 fi
 
 
 if [ "$build_part" = "all" ] || [ "$build_part" = "reconf" ] || [ "$build_part" = "chaos" ] || [ "$build_part" = "test" ]; then
-make -C test -j${make_jobs}
+    make -C test -j${make_jobs}
+    mret=$(echo $?)
+    if [ $mret -ne 0 ]; then
+        echo "COMPILE ERROR!!!!!!!!!!!!!!!!!!!!!"
+        exit
+    fi
 fi
